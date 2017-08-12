e82a23a1fd17819d0b9be0ff48b9685f02f3dae3
Dominik Schlaepfer <doemsche@gmx.ch>
2017-08-12 10:54:00 +0200

    implement oauth flow part 1

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "this is a call" });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ services/passport.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accesstoken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have a record with the given profiel ID
+          done(null, existingUser);
+        } else {
+          //we don't have a user record with this ID, make a new record
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
